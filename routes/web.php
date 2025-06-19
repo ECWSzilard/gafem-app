9e414be11a8c71e8760c0fc3fba8abb15a991d81
Szilárd <silimonbarabasszilard97@gmail.com>
2025-06-19 13:13:56 +0200

    Disabled CSRF token

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,49 @@
 <?php
 
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-$controller = \App\Http\Controllers\CommunicationController::class;
-
-Route::get('/order-data', [
-    $controller,
-    'orderData'
-])->name('order-data');
-
-Route::get('/products', [
-    $controller,
-    'products'
-])->name('products');
-
-Route::get('/ice-creams', [
-    $controller,
-    'iceCreams'
-])->name('ice-creams');
-
-Route::get('/servings', [
-    $controller,
-    'servings'
-])->name('servings');
-
-Route::get('/extra-options', [
-    $controller,
-    'extraOptions'
-])->name('extra-options');
-
-Route::post('/new-order',[
-   $controller,
-   'newOrder'
-])->name('new-order');
 
 
-Route::get('/today', [
-    $controller,
-    'todayOrders'
-])->name('today-orders');
+Route::middleware([
+    'web',
+    VerifyCsrfToken::class,
+])->group(function () {
+    $controller = \App\Http\Controllers\CommunicationController::class;
+
+    Route::get('/order-data', [
+        $controller,
+        'orderData'
+    ])->name('order-data');
+
+    Route::get('/products', [
+        $controller,
+        'products'
+    ])->name('products');
+
+    Route::get('/ice-creams', [
+        $controller,
+        'iceCreams'
+    ])->name('ice-creams');
+
+    Route::get('/servings', [
+        $controller,
+        'servings'
+    ])->name('servings');
+
+    Route::get('/extra-options', [
+        $controller,
+        'extraOptions'
+    ])->name('extra-options');
+
+    Route::post('/new-order', [
+        $controller,
+        'newOrder'
+    ])->name('new-order');
+
+
+    Route::get('/today', [
+        $controller,
+        'todayOrders'
+    ])->name('today-orders');
+});
