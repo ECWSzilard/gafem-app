bb20b2425ef949f91e64f4be230491443057403a
Szilárd <silimonbarabasszilard97@gmail.com>
2025-06-19 13:28:41 +0200

    Disabled CSRF token p4

--- routes/api.php
+++ routes/api.php
@@ -4,41 +4,49 @@ use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 
-    $controller = \App\Http\Controllers\CommunicationController::class;
 
-    Route::get('/order-data', [
-        $controller,
-        'orderData'
-    ])->name('order-data');
 
-    Route::get('/products', [
-        $controller,
-        'products'
-    ])->name('products');
+Route::prefix('api')
+    ->middleware('api')
+    ->group(function () {
 
-    Route::get('/ice-creams', [
-        $controller,
-        'iceCreams'
-    ])->name('ice-creams');
+        $controller = \App\Http\Controllers\CommunicationController::class;
 
-    Route::get('/servings', [
-        $controller,
-        'servings'
-    ])->name('servings');
+        Route::get('/order-data', [
+            $controller,
+            'orderData'
+        ])->name('order-data');
 
-    Route::get('/extra-options', [
-        $controller,
-        'extraOptions'
-    ])->name('extra-options');
+        Route::get('/products', [
+            $controller,
+            'products'
+        ])->name('products');
 
-    Route::post('/new-order', [
-        $controller,
-        'newOrder'
-    ])->name('new-order');
+        Route::get('/ice-creams', [
+            $controller,
+            'iceCreams'
+        ])->name('ice-creams');
 
-    Route::get('/today', [
-        $controller,
-        'todayOrders'
-    ])->name('today-orders');
+        Route::get('/servings', [
+            $controller,
+            'servings'
+        ])->name('servings');
+
+        Route::get('/extra-options', [
+            $controller,
+            'extraOptions'
+        ])->name('extra-options');
+
+        Route::post('/new-order', [
+            $controller,
+            'newOrder'
+        ])->name('new-order');
+
+        Route::get('/today', [
+            $controller,
+            'todayOrders'
+        ])->name('today-orders');
+
+    });
 
 
